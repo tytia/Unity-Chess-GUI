a492e05f7e60924812acdb0a84dd4a045895d71d
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-07 18:09:47 +1000

    Created Piece struct and PieceType enum to represent pieces in game logic

---
+++ Assets/ChessGame/Piece.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static Utility.Notation;
+
+namespace ChessGame
+{
+    public struct Piece
+    {
+        public Piece(PieceType type, SquarePos pos)
+        {
+            this.type = type;
+            this.pos = pos;
+        }
+        
+        public PieceType type { get; set; }
+        public SquarePos pos { get; set; }
+    }
+    
+    public enum PieceType : byte
+    {
+        Pawn = 1,
+        Knight = 2,
+        Bishop = 3,
+        Rook = 4,
+        Queen = 5,
+        King = 6,
+        White = 8,
+        Black = 16
+    }
+}
