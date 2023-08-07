b14db8b13f9e6f4cc11b3ce1bccf366f3431d14a
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-07 18:11:16 +1000

    Created Game class to handle states of the game

---
+++ Assets/ChessGame/Game.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using static Utility.Notation;
+
+namespace ChessGame
+{
+    public class Game
+    {
+        public Piece?[] board { get; set; } = new Piece?[64];
+        public SquarePos? enPassantPos { get; set; } = null;
+        public int halfMoveClock { get; set; } = 0;
+        public int fullMoveClock { get; set; } = 0;
+
+        public Game(string fen="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR")
+        {
+            LoadFEN(this, fen);
+        }
+    }
+}
