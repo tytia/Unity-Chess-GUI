8d185edf2fb4b9496fce4f2cce458f9f96e90824
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-09 03:07:55 +1000

    Reworked UpdateBoard() to use SquarePos.ToVector2() and implicit type declaration where evident

--- Assets/GUI/GameWindow/ColorPicker.cs
+++ Assets/GUI/GameWindow/ColorPicker.cs
@@ -6,40 +6,32 @@
 using UnityEngine;
 using static Utility.Notation;
 
-namespace GUI.GameWindow
-{
-    public class ColorPicker : MonoBehaviour
-    {
+namespace GUI.GameWindow {
+    public class ColorPicker : MonoBehaviour {
         [SerializeField] private Color _lightCol;
         [SerializeField] private Color _darkCol;
         [SerializeField] private Board _board;
 
-        private void Awake()
-        {
+        private void Awake() {
             _lightCol = Square.lightCol;
             _darkCol = Square.darkCol;
         }
 
-        private void OnValidate()
-        {
-            if (gameObject.activeSelf)
-            {
+        private void OnValidate() {
+            if (gameObject.activeSelf) {
                 UpdateBoard();
             }
         }
-        
-        private void UpdateBoard()
-        {
-            if (_board == null || _board.GetSquare(SquarePos.h8) == null)
-            {
+
+        private void UpdateBoard() {
+            if (_board == null || _board.GetSquare(SquarePos.h8) == null) {
                 return;
             }
-            
-            for (SquarePos pos = SquarePos.a1; pos <= SquarePos.h8; pos++)
-            {
-                int file = (int)pos % 8, rank = (int)pos / 8;
+
+            for (var pos = SquarePos.a1; pos <= SquarePos.h8; pos++) {
+                var point = pos.ToVector2();
                 Square sq = _board.GetSquare(pos);
-                sq.color = (file + rank) % 2 == 0 ? _darkCol : _lightCol;
+                sq.color = (point.x + point.y) % 2 == 0 ? _darkCol : _lightCol;
             }
         }
     }
