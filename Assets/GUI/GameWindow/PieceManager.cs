eea4197f6116adec4ab4208b5fa136bbfd6573cc
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-09 03:11:52 +1000

    Changed parent of initialized piece to the corresponding square it sits on

--- Assets/GUI/GameWindow/PieceManager.cs
+++ Assets/GUI/GameWindow/PieceManager.cs
@@ -3,48 +3,43 @@ using UnityEngine;
 using Chess;
 using Utility;
 
-namespace GUI.GameWindow
-{
-    public class PieceManager : MonoBehaviour
-    {
+namespace GUI.GameWindow {
+    public class PieceManager : MonoBehaviour {
         [SerializeField] private Piece _piece;
+        [SerializeField] private Board _board;
         [SerializeField] private Sprite[] _sprites;
         private Game _game;
-        
+
         public Sprite[] sprites => _sprites;
 
-        private void Awake()
-        {
+        private void Awake() {
             _game = new Game();
         }
 
-        private void Start()
-        {
+        private void Start() {
             InitPieces();
         }
 
-        private void InitPieces()
-        {
-            foreach (Chess.Piece piece in _game.pieces)
-            {
+        private void InitPieces() {
+            foreach (Chess.Piece piece in _game.pieces) {
                 var point = piece.pos.ToVector2();
-                Piece p = Instantiate(_piece, point, Quaternion.identity, transform);
+                Piece p = Instantiate(_piece, point, Quaternion.identity,
+                    _board.GetSquare(piece.pos).gameObject.transform);
                 p.SetSprite(PieceTypeToSprite(piece.type));
                 PieceType color = piece.type & (PieceType.White | PieceType.Black);
                 p.name = color + " " + (piece.type ^ color);
             }
         }
-        
-        private Sprite PieceTypeToSprite(PieceType type)
-        {
-            if (type.HasFlag(PieceType.White))
-            {
+
+        private Sprite PieceTypeToSprite(PieceType type) {
+            if (type.HasFlag(PieceType.White)) {
                 return sprites[(int)(type ^ PieceType.White)];
             }
-            if (type.HasFlag(PieceType.Black))
-            {
+
+            if (type.HasFlag(PieceType.Black)) {
                 return sprites[(int)(type ^ PieceType.Black) + 6];
             }
+
             throw new ArgumentException("Piece type must have a color");
         }
     }
