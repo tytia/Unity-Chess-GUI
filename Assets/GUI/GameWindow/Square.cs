319afbbfc200157a95656e07c0d3471aefc46f32
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-12 00:36:26 +1000

    Changed the dark square color to be more saturated

--- Assets/GUI/GameWindow/Square.cs
+++ Assets/GUI/GameWindow/Square.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 namespace GUI.GameWindow {
     public class Square : MonoBehaviour {
         public static Color lightCol { get; } = new Color32(231, 205, 193, 255);
-        public static Color darkCol { get; } = new Color32(168, 120, 107, 255);
+        public static Color darkCol { get; } = new Color32(181, 116, 97, 255);
         private SpriteRenderer _sr;
 
         public Color color {
