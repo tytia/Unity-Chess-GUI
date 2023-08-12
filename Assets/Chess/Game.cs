57fd7c47642d47d0ea67d4215a350698d0b52023
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-12 19:24:57 +1000

    Changed LoadFromFEN() to load strings with missing fields. Revised get & set accessors visibility

--- Assets/Chess/Game.cs
+++ Assets/Chess/Game.cs
@@ -8,37 +8,45 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Utility;
 using static Utility.Notation;
 
 namespace Chess {
     public class Game {
-        public Piece?[] board { get; } = new Piece?[64];
-
-        // TODO: Revise get & set accessors visibility
-        public List<Piece> pieces { get; private set; } = new(32);
-        public Side sideToMove { get; set; }
-        public CastlingRights castlingRights { get; set; }
-        public int? enPassantIndex { get; set; }
-        public int halfMoveClock { get; set; }
-        public int fullMoveClock { get; set; }
+        private readonly Piece?[] _board = new Piece?[64];
+        private readonly List<Piece> _pieces = new(32);
+        private Side _sideToMove;
+        private CastlingRights _castlingRights;
+        private int? _enPassantIndex;
+        private int _halfMoveClock;
+        private int _fullMoveClock;
+        
+        public ReadOnlyCollection<Piece> pieces => new(_pieces);
 
         public void CapturePiece(Piece piece) {
-            pieces.Remove(piece);
-            board[piece.index] = null;
+            _pieces.Remove(piece);
+            _board[piece.index] = null;
         }
 
-        public Game(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1") {
+        public Game(string fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR") {
             LoadFromFEN(fen);
         }
 
         private void LoadFromFEN(in string fen) {
             // GUI input field will be validated before calling this method
+            // game state defaults:
+            _sideToMove = Side.White;
+            _castlingRights = CastlingRights.All;
+            _enPassantIndex = null;
+            _halfMoveClock = 0;
+            _fullMoveClock = 1;
+            
             string[] fields = fen.Split(' ');
 
-            Array.Clear(board, 0, board.Length);
-            pieces.Clear();
+            Array.Clear(_board, 0, _board.Length);
+            _pieces.Clear();
             string[] ranks = fields[0].Split('/');
             for (int i = (int)SquarePos.a8, j = 0; j < 8; i -= 8, j++) {
                 for (int fileIndex = 0, file = 0; fileIndex < ranks[j].Length && file < 8; fileIndex++) {
@@ -47,39 +55,46 @@ namespace Chess {
                         file += c - '0';
                     }
                     else {
-                        pieces.Add(new Piece(Notation.charToPieceType[c], i + file));
-                        board[i + file] = pieces.Last();
+                        _pieces.Add(new Piece(Notation.charToPieceType[c], i + file));
+                        _board[i + file] = pieces.Last();
                         file++;
                     }
                 }
             }
 
-            sideToMove = fields[1] == "w" ? Side.White : Side.Black;
-
-            castlingRights = CastlingRights.None;
+            if (fields.Length < 2) return;
+            _sideToMove = fields[1] == "w" ? Side.White : Side.Black;
+            
+            if (fields.Length < 3) return;
+            _castlingRights = CastlingRights.None;
             if (fields[2] != "-") {
                 foreach (char c in fields[2]) {
-                    castlingRights |= Notation.charToCastlingRights[c];
+                    _castlingRights |= Notation.charToCastlingRights[c];
                 }
             }
-
-            enPassantIndex = fields[3] == "-" ? null : (int)Enum.Parse<SquarePos>(fields[3]);
-            halfMoveClock = Int32.Parse(fields[4]);
-            fullMoveClock = Int32.Parse(fields[5]);
+            
+            if (fields.Length < 4) return;
+            _enPassantIndex = fields[3] == "-" ? null : (int)Enum.Parse<SquarePos>(fields[3]);
+            
+            if (fields.Length < 5) return;
+            _halfMoveClock = Int32.Parse(fields[4]);
+            
+            _fullMoveClock = fields.Length < 6 ? (_halfMoveClock / 2) + 1 : Int32.Parse(fields[5]);
         }
     }
 
-    public enum Side {
+    public enum Side : byte {
         White,
         Black
     }
 
     [Flags]
-    public enum CastlingRights {
+    public enum CastlingRights : byte {
         None = 0,
         WhiteKingSide = 1,
         WhiteQueenSide = 2,
         BlackKingSide = 4,
-        BlackQueenSide = 8
+        BlackQueenSide = 8,
+        All = WhiteKingSide | WhiteQueenSide | BlackKingSide | BlackQueenSide
     }
 }
