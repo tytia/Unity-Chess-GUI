b66ee22939731a8bfe2d3e18f336b7e14da3c58d
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-23 02:10:03 +1000

    Moved highlight squares' initialization to HighlightManager (formerly known as SquareHighlighter)

--- Assets/GUI/GameWindow/Board.cs
+++ Assets/GUI/GameWindow/Board.cs
@@ -13,24 +13,19 @@ namespace GUI.GameWindow {
 
         private void Awake() {
             InitBoard();
+            HighlightManager.InitHighlights(_highlight);
         }
 
         private void InitBoard() {
-            var highlights = new Square[64];
-            Transform highlightsParent = Instantiate(new GameObject("Highlights"), transform).transform;
             for (var pos = SquarePos.a1; pos <= SquarePos.h8; pos++) {
                 var point = pos.ToVector2();
                 Square sq = Instantiate(_square, point, Quaternion.identity, transform);
-                Square sqHighlight = Instantiate(_highlight, point, Quaternion.identity, highlightsParent);
                 sq.color = (point.x + point.y) % 2 == 0 ? Square.darkCol : Square.lightCol;
-                sq.name = sqHighlight.name = pos.ToString();
                 sq.name = pos.ToString();
 
                 _squares[(int)pos] = sq;
-                highlights[(int)pos] = sqHighlight;
             }
 
-            SquareHighlighter.highlights = highlights;
             _cam.position = new Vector3((float)8 / 2 - 0.5f, (float)8 / 2 - 0.5f, _cam.position.z);
         }
 
