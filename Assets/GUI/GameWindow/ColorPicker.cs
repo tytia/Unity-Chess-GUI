f79c8e6b4debbbd986590428e77938b25f53792b
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-23 02:32:24 +1000

    Added functionality to pick squares' legal moves color

--- Assets/GUI/GameWindow/ColorPicker.cs
+++ Assets/GUI/GameWindow/ColorPicker.cs
@@ -4,22 +4,25 @@
  */
 
 using UnityEngine;
-using static Utility.Notation;
+using static GUI.GameWindow.HighlightManager;
 
 namespace GUI.GameWindow {
     public class ColorPicker : MonoBehaviour {
         [SerializeField] private Color _lightCol;
         [SerializeField] private Color _darkCol;
+        [SerializeField] private Color _legalMovesCol;
         [SerializeField] private Board _board;
 
         private void Awake() {
             _lightCol = Square.lightCol;
             _darkCol = Square.darkCol;
+            _legalMovesCol = Square.legalMovesCol;
         }
 
         private void OnValidate() {
-            if (gameObject.activeSelf && _board != null && _board.GetSquare((int)SquarePos.h8) != null) {
+            if (gameObject.activeSelf) {
                 UpdateSquares();
+                UpdateHighlights();
             }
         }
 
@@ -27,6 +30,14 @@ namespace GUI.GameWindow {
             foreach (Square sq in _board.squares) {
                 Vector2 worldPos = sq.transform.position;
                 sq.color = (worldPos.x + worldPos.y) % 2 == 0 ? _darkCol : _lightCol;
+            }
+        }
+
+        private void UpdateHighlights() {
+            foreach (Square sq in highlights) {
+                if (sq.isActiveAndEnabled && sq.color != Square.prevMoveCol) {
+                    sq.color = _legalMovesCol;
+                }
             }
         }
     }
