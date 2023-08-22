c962b64c5f6e0492de602bc8761b512926404143
Terry Tian <97772315+tytia@users.noreply.github.com>
2023-08-23 02:30:55 +1000

    Implemented highlighting legal moves on piece selection and enforcing those legal moves

--- Assets/GUI/GameWindow/MoveHandler.cs
+++ Assets/GUI/GameWindow/MoveHandler.cs
@@ -2,13 +2,14 @@ using Chess;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using Utility;
+using static GUI.GameWindow.HighlightManager;
 
 namespace GUI.GameWindow {
     public class MoveHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler {
         private SpriteRenderer _sr;
-        private Piece _piece;
         private Camera _cam;
         private Game _game;
+        private Piece _piece;
 
         private void Awake() {
             _sr = GetComponent<SpriteRenderer>();
@@ -20,13 +21,15 @@ namespace GUI.GameWindow {
             // need to wait for piece to get properly initialised before getting it,
             // which is why this is in Start() and not Awake()
             _piece = GetComponent<PieceGUI>().piece;
-            if (_piece.color != _game.playerColor) {
+            _game.analysisMode = true; // TODO: temporarily turned on for development, remove this later
+            if (_piece.color != _game.playerColor && !_game.analysisMode) {
                 enabled = false;
             }
         }
 
         public void OnPointerDown(PointerEventData eventData) {
             _sr.sortingOrder = 1;
+            HighlightLegalMoves(_piece);
             MoveToMouse();
         }
 
@@ -35,7 +38,13 @@ namespace GUI.GameWindow {
         }
 
         public void OnPointerUp(PointerEventData eventData) {
-            MoveToNearestSquare();
+            Square nearestSquare = MoveToNearestSquare();
+            if (highlightedPiece.Equals(_piece) && nearestSquare.transform == transform.parent) {
+                UnHighlightLegalMoves();
+            }
+            else {
+                highlightedPiece = _piece;
+            }
         }
 
         private void MoveToMouse() {
@@ -44,23 +53,27 @@ namespace GUI.GameWindow {
             transform.position = _cam.ScreenToWorldPoint(mousePos);
         }
 
-        private void MoveToNearestSquare() {
+        private Square MoveToNearestSquare() {
             Vector2 mousePosWorld = _cam.ScreenToWorldPoint(Input.mousePosition);
             Collider2D squareCollider = Physics2D.OverlapPoint(mousePosWorld, LayerMask.GetMask("Board"));
+            // assign an impossible index if collider is null
+            int to = squareCollider != null ? squareCollider.transform.position.ToBoardIndex() : -1;
 
-            if (squareCollider != null && squareCollider.transform != transform.parent) {
+            if (_piece.GetLegalSquares().Contains(to)) {
                 foreach (PieceGUI p in squareCollider.GetComponentsInChildren<PieceGUI>()) {
                     Destroy(p.gameObject);
                 }
 
                 transform.parent = squareCollider.transform;
-                int to = squareCollider.transform.position.ToBoardIndex();
-                SquareHighlighter.HighlightMove(_piece.index, to);
+                HighlightMove(_piece.index, to); // needs to be before MovePiece() because the piece's index will change
                 _game.MovePiece(ref _piece, to);
+                UnHighlightLegalMoves();
             }
 
             transform.position = transform.parent.position;
             _sr.sortingOrder = 0;
+            
+            return squareCollider.GetComponent<Square>();
         }
     }
 }
