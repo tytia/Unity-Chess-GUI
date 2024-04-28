93f36fd9ceab75a324a006480eb805bce0bb4e01
Terry Tian <97772315+tytia@users.noreply.github.com>
2024-04-28 17:30:01 +1000

    Added button for user to load their engine of choice and UCIThread class for communication between the GUI and the selected engine

---
+++ Assets/GUI/GameWindow/EngineLoader.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using Chess;
+using SFB;
+using UnityEngine;
+
+namespace GUI.GameWindow {
+    public class EngineLoader : MonoBehaviour {
+        [SerializeField] private GameObject _invalidPathText;
+
+        private void Awake() {
+            _invalidPathText.SetActive(!File.Exists(PlayerPrefs.GetString("engine_path")));
+            Debug.Log(PlayerPrefs.GetString("engine_path"));
+        }
+
+        public void LoadEngine() {
+            string directory = PlayerPrefs.HasKey("engine_path") ? Path.GetDirectoryName(PlayerPrefs.GetString("engine_path")) : "";
+            string[] paths = StandaloneFileBrowser.OpenFilePanel("Select engine", directory, "exe", false);
+            if (paths.Length != 0) {
+                PlayerPrefs.SetString("engine_path", paths[0]);
+                UCIThread.engine_path = paths[0];
+                _invalidPathText.SetActive(false);
+            }
+        }
+    }
+}
