1d346dc8665dcbae5ac760f30937333f5c882888
Terry Tian <97772315+tytia@users.noreply.github.com>
2024-05-06 15:14:45 +1000

    Added Reader class for that allows for adding a timeout to readline. Code from https://stackoverflow.com/questions/57615/how-to-add-a-timeout-to-console-readline

---
+++ Assets/Chess/Util/Reader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace Chess.Util {
+    public static class Reader {
+        private static readonly AutoResetEvent _getInput, _gotInput;
+        private static string _input;
+
+        static Reader() {
+            _getInput = new AutoResetEvent(false);
+            _gotInput = new AutoResetEvent(false);
+            var inputThread = new Thread(Read) {
+                IsBackground = true
+            };
+            inputThread.Start();
+        }
+
+        private static void Read() {
+            while (true) {
+                _getInput.WaitOne();
+                _input = Console.ReadLine();
+                _gotInput.Set();
+            }
+        }
+        
+        public static bool TryReadLine(out string line, int timeout = Timeout.Infinite) {
+            _getInput.Set();
+            bool success = _gotInput.WaitOne(timeout);
+            
+            line = success ? _input : null;
+            return success;
+        }
+    }
+}
